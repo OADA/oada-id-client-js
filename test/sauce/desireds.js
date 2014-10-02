0a2038a1c2005d8b84722606bb255188f1287f04
Alex Layton <alex@layton.in>
2014-10-01 21:09:11 -0400

    Add even more Sauce platforms

--- test/sauce/desireds.js
+++ test/sauce/desireds.js
@@ -33,13 +33,106 @@ module.exports = {
         browserName: 'chrome',
         platform: 'os x 10.6'
     },
+    'linux-firefox': {
+        browserName: 'firefox',
+        platform: 'linux'
+    },
+    'winxp-firefox': {
+        browserName: 'firefox',
+        platform: 'windows xp'
+    },
+    'win7-firefox': {
+        browserName: 'firefox',
+        platform: 'windows 7'
+    },
+    'win8-firefox': {
+        browserName: 'firefox',
+        platform: 'windows 8'
+    },
+    'win8.1-firefox': {
+        browserName: 'firefox',
+        platform: 'windows 8.1'
+    },
+    'osx10.9-firefox': {
+        browserName: 'firefox',
+        platform: 'os x 10.9'
+    },
+    'osx10.8-firefox': {
+        browserName: 'firefox',
+        platform: 'os x 10.8'
+    },
+    'osx10.6-firefox': {
+        browserName: 'firefox',
+        platform: 'os x 10.6'
+    },
+    'linux-explorer': {
+        browserName: 'internet explorer',
+        platform: 'linux'
+    },
+    'winxp-explorer': {
+        browserName: 'internet explorer',
+        platform: 'windows xp'
+    },
+    'win7-explorer': {
+        browserName: 'internet explorer',
+        platform: 'windows 7'
+    },
+    'win8-explorer': {
+        browserName: 'internet explorer',
+        platform: 'windows 8'
+    },
+    'win8.1-explorer': {
+        browserName: 'internet explorer',
+        platform: 'windows 8.1'
+    },
+    'osx10.9-explorer': {
+        browserName: 'internet explorer',
+        platform: 'os x 10.9'
+    },
+    'osx10.8-explorer': {
+        browserName: 'internet explorer',
+        platform: 'os x 10.8'
+    },
+    'osx10.6-explorer': {
+        browserName: 'internet explorer',
+        platform: 'os x 10.6'
+    },
+    'linux-safari': {
+        browserName: 'safari',
+        platform: 'linux'
+    },
+    'winxp-safari': {
+        browserName: 'safari',
+        platform: 'windows xp'
+    },
+    'win7-safari': {
+        browserName: 'safari',
+        platform: 'windows 7'
+    },
+    'win8-safari': {
+        browserName: 'safari',
+        platform: 'windows 8'
+    },
+    'win8.1-safari': {
+        browserName: 'safari',
+        platform: 'windows 8.1'
+    },
+    'osx10.9-safari': {
+        browserName: 'safari',
+        platform: 'os x 10.9'
+    },
+    'osx10.8-safari': {
+        browserName: 'safari',
+        platform: 'os x 10.8'
+    },
+    'osx10.6-safari': {
+        browserName: 'safari',
+        platform: 'os x 10.6'
+    },
     'android': {
         browserName: 'android'
     },
     'iphone': {
         browserName: 'iphone'
-    },
-    'firefox': {browserName: 'firefox'},
-    'safari': {browserName: 'safari'},
-    'explorer': {browserName: 'internet explorer'}
+    }
 };
