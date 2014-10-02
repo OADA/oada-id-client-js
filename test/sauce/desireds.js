8a124cbaccba68e67a85bf52e47c9a2203e0d4c8
Alex Layton <alex@layton.in>
2014-10-01 20:00:28 -0400

    Attempt to add more Sauce platforms

--- test/sauce/desireds.js
+++ test/sauce/desireds.js
@@ -1,8 +1,45 @@
 'use strict';
 
 module.exports = {
-    chrome: {browserName: 'chrome'},
-    firefox: {browserName: 'firefox'},
-    safari: {browserName: 'safari'},
-    explorer: {browserName: 'internet explorer'}
+    'linux-chrome': {
+        browserName: 'chrome',
+        platform: 'linux'
+    },
+    'winxp-chrome': {
+        browserName: 'chrome',
+        platform: 'windows xp'
+    },
+    'win7-chrome': {
+        browserName: 'chrome',
+        platform: 'windows 7'
+    },
+    'win8-chrome': {
+        browserName: 'chrome',
+        platform: 'windows 8'
+    },
+    'win8.1-chrome': {
+        browserName: 'chrome',
+        platform: 'windows 8.1'
+    },
+    'osx10.9-chrome': {
+        browserName: 'chrome',
+        platform: 'os x 10.9'
+    },
+    'osx10.8-chrome': {
+        browserName: 'chrome',
+        platform: 'os x 10.8'
+    },
+    'osx10.6-chrome': {
+        browserName: 'chrome',
+        platform: 'os x 10.6'
+    },
+    'android': {
+        browserName: 'android'
+    },
+    'iphone': {
+        browserName: 'iphone'
+    },
+    'firefox': {browserName: 'firefox'},
+    'safari': {browserName: 'safari'},
+    'explorer': {browserName: 'internet explorer'}
 };
