a5754948c660f0c4d0962e75e5b2e0d7dd266d81
Andrew Balmos <andrew@balmos.org>
2016-03-14 13:57:48 -0400

    Update metadata

--- examples/server-client/server-client.js
+++ examples/server-client/server-client.js
@@ -28,10 +28,27 @@ var kid = '1234';
 var key = {pem:pem, kid:kid};
 
 var options = {
-    metadata: {
-        'client_id': 'jf93caauf3uzud7f308faesf3@identity.oada-dev.com',
-        'redirect_uri': 'http://localhost:3007/redirect'
-    },
+    metadata: 'eyJqa3UiOiJodHRwczovL2lkZW50aXR5Lm9hZGEtZGV2LmNvbS9jZXJ0cyIsI' +
+              'mtpZCI6ImtqY1NjamMzMmR3SlhYTEpEczNyMTI0c2ExIiwidHlwIjoiSldUIi' +
+              'wiYWxnIjoiUlMyNTYifQ.eyJyZWRpcmVjdF91cmlzIjpbImh0dHA6Ly9sb2Nh' +
+              'bGhvc3Q6MzAwNy9yZWRpcmVjdCJdLCJ0b2tlbl9lbmRwb2ludF9hdXRoX21ld' +
+              'GhvZCI6InVybjppZXRmOnBhcmFtczpvYXV0aDpjbGllbnQtYXNzZXJ0aW9uLX' +
+              'R5cGU6and0LWJlYXJlciIsImdyYW50X3R5cGVzIjpbImltcGxpY2l0Il0sInJ' +
+              'lc3BvbnNlX3R5cGVzIjpbInRva2VuIiwiaWRfdG9rZW4iLCJpZF90b2tlbiB0' +
+              'b2tlbiJdLCJjbGllbnRfbmFtZSI6Ik9BREEgSUQgRXhhbXBsZSBCcm93c2VyI' +
+              'ENsaWVudCIsImNsaWVudF91cmkiOiJodHRwOi8vbG9jYWxob3N0OjMwMDciLC' +
+              'Jjb250YWN0cyI6WyJJbmZvIDxpbmZvQG9wZW5hZy5pbz4iXSwicG9saWN5X3V' +
+              'yaSI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwNy9wdWMuaHRtbCIsInRvc191cmki' +
+              'OiJodHRwOi8vbG9jYWxob3N0OjMwMDcvcHVjLmh0bWwiLCJsaWNlbnNlcyI6W' +
+              '3siaWQiOiJvYWRhLTEuMCIsIm5hbWUiOiJPQURBIEV4YW1wbGUgdjEuMCJ9LH' +
+              'siaWQiOiJvYWRhLTIuMCIsIm5hbWUiOiJPQURBIEV4YW1wbGUgdjIuMCJ9XSw' +
+              'iandrc191cmkiOiJodHRwOi8vbG9jYWxob3N0OjMwMDcvY2VydHMiLCJzb2Z0' +
+              'd2FyZV9pZCI6ImQxY2Y0NjliLTg4MTAtNDYyNC1iMGY1LWM4ZTRjYWVlMWM5Z' +
+              'SIsInJlZ2lzdHJhdGlvbl9wcm9pdmRlciI6Imh0dHBzOi8vaWRlbnRpdHkub2' +
+              'FkYS1kZXYuY29tIiwiaWF0IjoxNDU3OTc0MjYzfQ.AGSRLggTeubCJGwBirmi' +
+              'etxWTShB2CDTy9eQZFM5rNB6sKidfqkMyyJd7JlaWSiHagTP4Sacb-8J1CkjD' +
+              '2Fd-mKB0CJ-QARWq_hlZDrJmjHN1q6JQv7XUZcKxr926vWliWvmGVijGUKzUW' +
+              '9pQZGWW5i7y3xcgZf5Lm3fNSNks7I',
     scope: 'bookmarks.machines.harvesters',
     params: {
         prompt: 'consent'
