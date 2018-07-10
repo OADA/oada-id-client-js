117eb5fe98b180a105d400532a1d41753775b731
sanoel <noel.samuel.a@gmail.com>
2018-07-09 22:04:43 -0400

    added browserify to oauth2 directory

--- src/node-browser.js
+++ src/node-browser.js
@@ -1,29 +1,29 @@
-const opn = require('opn');
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
+'use strict';
+var opn = require('opn');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var Promise = require('bluebird');
 
 function hostLogin(domain, options) {
-    return new Promise((resolve, reject) => {
-        const server = http.createServer(async (req, res) => {
+    return new Promise(function(resolve) {
+        var server = http.createServer(function(req, res) {
 
             if (/^\/somefile/.test(req.url)) {
-                var urlObj = url.parse(req.url, true)
+                var urlObj = url.parse(req.url, true);
                 server.close();
-                return resolve({access_token: urlObj.query.access_token})
+                return resolve({access_token: urlObj.query.access_token});
             }
-
-            let f = req.url.split('?')[0];
-            let file = path.join(__dirname, '/public/'+f);
+            var f = req.url.split('?')[0];
+            var file = path.join(__dirname, '/public/' + f);
             res.writeHead(200, {
                 'Content-Length': fs.statSync(file).size
             });
             var readStream = fs.createReadStream(file);
             readStream.pipe(res);
-
-        }).listen(8000, () => {
-            opn(`http://localhost:8000/index.html?domain=${JSON.stringify(domain)}&options=${JSON.stringify(options)}`)
+        }).listen(8000, function() {
+            opn('http://localhost:8000/index.html?domain=' + JSON.stringify(domain) + '&options=' + JSON.stringify(options));
         })
     });
 }
