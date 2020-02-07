99a83beee06ea9ecbb71a67113f4863fa9ffd17b
Alex Layton <alex@layton.in>
2020-02-07 17:56:04 -0500

    Fix opening links in browser on Windows

--- src/node-browser.js
+++ src/node-browser.js
@@ -1,38 +1,45 @@
-'use strict';
-var opn = require('open');
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var Promise = require('bluebird');
+'use strict'
+var opn = require('open')
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var path = require('path')
+var Promise = require('bluebird')
 
-function hostLogin(domain, options) {
-    return new Promise(function(resolve) {
-        var server = http.createServer(function(req, res) {
+function hostLogin (domain, options) {
+  return new Promise(function (resolve) {
+    var server = http
+      .createServer(function (req, res) {
+        if (/favicon/.test(req.url)) {
+          res.statusCode = 404
+          res.statusMessage = 'Not found'
+          res.end()
+          return
+        }
 
-            if (/favicon/.test(req.url)) {
-                res.statusCode = 404;
-                res.statusMessage = "Not found";
-                res.end();
-                return;
-            }
-
-            if (/^\/somefile/.test(req.url)) {
-                var urlObj = url.parse(req.url, true);
-                server.close();
-                return resolve({access_token: urlObj.query.access_token});
-            }
-            var f = req.url.split('?')[0];
-            var file = path.join(__dirname, '/public/' + f);
-            res.writeHead(200, {
-                'Content-Length': fs.statSync(file).size
-            });
-            var readStream = fs.createReadStream(file);
-            readStream.pipe(res);
-        }).listen(8000, function() {
-            opn('http://localhost:8000/index.html?domain=' + JSON.stringify(domain) + '&options=' + JSON.stringify(options));
+        if (/^\/somefile/.test(req.url)) {
+          var urlObj = url.parse(req.url, true)
+          server.close()
+          return resolve({ access_token: urlObj.query.access_token })
+        }
+        var f = req.url.split('?')[0]
+        var file = path.join(__dirname, '/public/' + f)
+        res.writeHead(200, {
+          'Content-Length': fs.statSync(file).size
         })
-    });
+        var readStream = fs.createReadStream(file)
+        readStream.pipe(res)
+      })
+      .listen(8000, function () {
+        opn(
+          'http://localhost:8000/index.html?domain=' +
+            JSON.stringify(domain) +
+            '&options=' +
+            JSON.stringify(options),
+          { url: true }
+        )
+      })
+  })
 }
 
-module.exports = hostLogin;
+module.exports = hostLogin
